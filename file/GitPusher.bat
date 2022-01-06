3af80e8e498bdce033ec9bdd83323da90c5e57ac
Xujiayao <caiandxu@163.com>
2022-01-06 23:57:30 +0800

    Site updated: 2022-01-06 23:57:29

--- file/GitPusher.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Git Pusher By Xujiayao
-cd C:\Users\Xujiayao\Desktop\Blog\source\file
-git add .
-echo Enter commit message: 
-set /p commit_message=
-git commit -m "%commit_message%"
-git push origin master
-pause
